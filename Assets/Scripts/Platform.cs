ce13fad050990abd4ea40524a892511092a45a46
wjddn8038 <63216655+wjddn8038@users.noreply.github.com>
2020-07-20 14:06:08 +0900

    7/20
    run game 완성

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
 // 발판으로서 필요한 동작을 담은 스크립트
 public class Platform : MonoBehaviour {
@@ -8,9 +9,24 @@ public class Platform : MonoBehaviour {
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
     private void OnEnable() {
         // 발판을 리셋하는 처리
+        stepped = false;
+
+        for(int i=0;i<obstacles.Length;i++)
+        {
+            if(Random.Range(0,3)==0)
+            {
+                obstacles[i].SetActive(true);
+            }
+            else
+            {
+                obstacles[i].SetActive(false);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        stepped = true;
+        GameManager.instance.AddScore(1);
     }
 }
